e6eb0b63a67e9bf1594789b931ecf9af734afb31
KTD-FGC <camerontheauthor@gmail.com>
2022-08-24 15:12:20 -0400

    Zork 1.1 Initial commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,21 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork");
+
+            string inputstring = Console.ReadLine().Trim();
+            inputstring = inputstring.ToUpper();
+            if (inputstring == "QUIT")
+            {
+                Console.WriteLine("Thank you for Playing");
+            }
+            else if (inputstring == "LOOK")
+            {
+                Console.WriteLine("There is an open field West of a whit house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door");
+            }
+            else
+            {
+                Console.WriteLine($"Unrecognized command: {inputstring}");
+            }
         }
     }
 }
