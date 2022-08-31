b651a22d3ba7b4db15734594d913686a7aff369b
KTD-FGC <camerontheauthor@gmail.com>
2022-08-31 15:15:19 -0400

    Zork 1.3 Classwork-1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,48 +8,41 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork");
 
-            string inputstring = Console.ReadLine().Trim().ToUpper();
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Write("> ");
+                string inputstring = Console.ReadLine().Trim().ToUpper();
+                Commands command = ToCommand(inputstring);
 
-            Commands command = ToCommand(inputstring);
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        isRunning = false;
+                        outputString = "Thank you for playing!";
+                        break;
 
-            if (command == Commands.QUIT)
-            {
-                Console.WriteLine("Thank you for Playing");
-            }
-            else if (command == Commands.LOOK)
-            {
-                Console.WriteLine("There is an open field West of a whit house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door");
-            }
-            else if (command == Commands.NORTH)
-            {
-                Console.WriteLine("You go North");
-            }
-            else if (command == Commands.SOUTH)
-            {
-                Console.WriteLine("You go South");
-            }
-            else if (command == Commands.WEST)
-            {
-                Console.WriteLine("You go West");
-            }
-            else if (command == Commands.EAST)
-            {
-                Console.WriteLine("You go East");
-            }
-            else
-            {
-                Console.WriteLine($"Unrecognized command: {inputstring}");
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.WEST:
+                    case Commands.EAST:
+                        outputString = $"You moved {command.ToString()}.";
+                        break;
+
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
             }
         }
 
-        static Commands ToCommand(string commandString)
-        {
-            return Enum.TryParse<Commands>(commandString, true, out Commands command) ? command : Commands.UNKNOWN;
-        }
-
-        static bool IsEven(int value)
-        {
-            return value % 2 == 0 ? true : false;
-        }
+        static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands command) ? command : Commands.UNKNOWN;
     }
 }
